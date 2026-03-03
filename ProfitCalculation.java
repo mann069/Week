21da92326eeb6fe33521d632f205a76b7ba522b6
mann069 <ms8226@srmist.edu.in>
2026-03-03 15:40:34 +0530

    Add files via upload

---
+++ ProfitCalculation.java
@@ -0,0 +1,20 @@
+// Program to calculate profit and profit percentage
+
+class ProfitCalculation {
+    public static void main(String[] args) {
+
+        // Price variables
+        double costPrice = 129;
+        double sellingPrice = 191;
+
+        // Profit calculation
+        double profit = sellingPrice - costPrice;
+        double profitPercentage = (profit / costPrice) * 100;
+
+        // Display result
+        System.out.println("The Cost Price is INR " + costPrice +
+                " and Selling Price is INR " + sellingPrice +
+                "\nThe Profit is INR " + profit +
+                " and the Profit Percentage is " + profitPercentage + "%");
+    }
+}
