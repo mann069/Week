f7ea443b80166fede4a5cb4efa4362c1b05d9461
mann069 <ms8226@srmist.edu.in>
2026-03-03 15:17:33 +0530

    Weekly assignment problem 1

---
+++ HarryAge.java
@@ -0,0 +1,14 @@
+public class HarryAge{
+    public static void main(String[] args) {
+        // Declaring variables
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculating age
+        int age = currentYear - birthYear;
+
+        // Display result
+        System.out.println(name + "'s age in " + currentYear + " is " + age);
+}
+}
